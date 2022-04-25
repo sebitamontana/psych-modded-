f78fe35c28e8248dc870b00df6d0af75383f7da7
sebitamontana <93792473+sebitamontana@users.noreply.github.com>
2022-04-25 15:13:23 -0400

    added camara follow pos

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -110,8 +110,8 @@ class FunkinLua {
 		set('week', WeekData.weeksList[PlayState.storyWeek]);
 		set('seenCutscene', PlayState.seenCutscene);
 		
-		// Block require and os, Should probably have a proper function but this should be good enough for now until someone smarter comes along and recreates a safe version of the OS library
-		set('require', false);
+		// os is readded
+
 
 		// Camera poo
 		set('cameraX', 0);
